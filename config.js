6b328c3ec91a246ee7b2a4b5d10e377aaf3628ce
Oleg <o.yanchinskiy@gmail.com>
2015-05-26 13:55:15 -0700

    updated config

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'blog.oleg24.com',
+        url: 'http://yoleg.azurewebsites.net',
         mail: {},
         database: {
             client: 'sqlite3',
